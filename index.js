5252c20322ae1a549d7ef21b76d942667ee6dd41
Денис Таньков <denis_tankoff@mail.ru>
2023-01-30 22:55:21 +0700

    fixed

--- index.js
+++ index.js
@@ -1,39 +1,4 @@
+#!/usr/bin/env node
 import _ from 'lodash';
 
-const freeEmailDomains = [
-  'gmail.com',
-  'yandex.ru',
-  'hotmail.com',
-  'yahoo.com',
-];
-
-const emails = [
-    'info@gmail.com',
-    'info@yandex.ru',
-    'info@hotmail.com',
-    'mk@host.com',
-    'support@hexlet.io',
-    'key@yandex.ru',
-    'sergey@gmail.com',
-    'vovan@gmail.com',
-    'vovan@hotmail.com',
-];
-
-const getFreeDomainsCount = (emails) => {
-  const splitedEmails  = [];
-  for (let email of emails) {
-    const parts = email.split('@');
-    email = _.last(parts);
-    splitedEmails.push(email);
-  }
-  
-  const filteredDomains = splitedEmails.filter((domain) =>      freeEmailDomains.includes(domain));
-  //console.log(filteredDomains);
-
-  const count = filteredDomains.reduce( (tally, domain) => {
-  tally[domain] = (tally[domain] || 0) + 1 ;
-  return tally;
-  } , {})
-  return count;
-}
-console.log(getFreeDomainsCount(emails));    
+console.log(_.chunk(['a', 'b', 'c', 'd'], 2));
